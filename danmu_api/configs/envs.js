5e9a373fdf6ac5f7cee19a1ed2153055504a13b1
bkjbggemo <zijiewang614@gmail.com>
2025-12-21 15:33:52 +0800

    Update envs.js

--- danmu_api/configs/envs.js
+++ danmu_api/configs/envs.js
@@ -251,7 +251,7 @@ export class Envs {
       vodAllowedPlatforms: this.VOD_ALLOWED_PLATFORMS,
       allowedPlatforms: this.ALLOWED_PLATFORMS,
       token: this.get('TOKEN', '87654321', 'string', true), // token，默认为87654321
-      adminToken: this.get('ADMIN_TOKEN', '87654321', 'string', true), // admin token，用于系统管理访问控制
+      adminToken: this.get('ADMIN_TOKEN', '1234567', 'string', true), // admin token，用于系统管理访问控制
       sourceOrderArr: this.resolveSourceOrder(), // 源排序
       otherServer: this.get('OTHER_SERVER', 'https://api.danmu.icu', 'string'), // 第三方弹幕服务器
       vodServers: this.resolveVodServers(), // vod站点配置，格式：名称@URL,名称@URL
